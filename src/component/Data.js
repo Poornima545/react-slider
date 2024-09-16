03ffa86def61d200bb0ae4afa09cc2d9ce674026
Poornima545 <poornimac714@gmail.com>
2024-09-16 12:47:47 +0530

    adding slider project

---
+++ src/component/Data.js
@@ -0,0 +1,26 @@
+export const Data = [
+    {
+        img: "https://www.course-api.com/images/people/person-1.jpeg",
+        name: 'maria ferguson',
+        title: 'Office Manager',
+        description: 'Fingerstache umami squid, kinfolk subway tile selvage tumblr man braid viral kombucha gentrify fanny pack raclette pok pok mustache.'
+    },
+    {
+        img: "https://www.course-api.com/images/people/person-4.jpeg",
+        name: 'John doe',
+        title: 'Regular guy',
+        description: 'Gastropub sustainable tousled prism occupy. Viral XOXO roof party brunch actually, chambray listicle microdosing put a bird on it paleo subway tile squid umami.'
+    },
+    {
+        img: "https://www.course-api.com/images/people/person-3.jpeg",
+        name: 'Peter smith',
+        title: 'product enginer',
+        description: 'Drinking vinegar polaroid street art echo park, actually semiotics next level butcher master cleanse hammock flexitarian ethical paleo.'
+    },
+    {
+        img: "https://www.course-api.com/images/people/person-2.jpeg",
+        name: 'susan andersen',
+        title: 'The boss',
+        description: 'Marfa af yr 3 wolf moon kogi, readymade distillery asymmetrical seitan kale chips fingerstache cloud bread mustache twee messenger bag.'
+    },
+]
